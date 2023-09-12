f88adc17711f8040d201b692a313a3cccadf570b
Clarence Grady <clarence.grady@ui.ac.id>
2023-09-12 14:41:47 +0700

    new update

--- main/models.py
+++ main/models.py
@@ -8,6 +8,8 @@ class Product(models.Model):
     price = models.IntegerField()
     cat=models.TextField()
     pub=models.TextField()
+    nama=models.CharField(max_length=255)
+    kelas=models.TextField()
     
 
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 def show_main(request):
     context = {
-        'app':'Game Collections',
+        'app1':'Game ',
+        'app2':'Collections',
         'name': 'Cyberpunk 2077',
         'amount':'1',
         'desc':'An open-world, action-adventure RPG set in the megalopolis of Night City, where you play as a cyberpunk mercenary wrapped-up in a do-or-die fight for survival.',
         'price':'59.99',
         'cat':'Action RPG, Open World',
-        'pub':'CD PROJEKT RED'
+        'pub':'CD PROJEKT RED',
+        'nama':'Clarence Grady',
+        'kelas':'PBP A'
 
     }
 
